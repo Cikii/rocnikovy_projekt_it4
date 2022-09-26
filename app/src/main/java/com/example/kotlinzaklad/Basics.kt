ec4bb4ffcbb14d42327197011677c67714786ac1
Cikii <cikislender@gmail.com>
2022-09-26 20:28:00 +0200

    Kotlin Zaklady
    Trenuju vite co

---
+++ app/src/main/java/com/example/kotlinzaklad/Basics.kt
@@ -0,0 +1,21 @@
+package com.example.kotlinzaklad
+
+fun main(){
+    //string
+    var myName = "Viktor"
+
+
+    var myAge = 18
+    var bub = 16
+
+    var jes = true
+
+
+    println("Hello $myName your name has ${myName.length} characters")
+
+    val isEqual = 5>3
+    println("isEqual is $isEqual")
+    println("is 5 greater than 3 --> ${5>3}")
+
+}
+
